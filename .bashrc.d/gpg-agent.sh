f9ae817ec8bc489aca3e743c86c272184585bbb8
Michael Carlberg <c@rlberg.se>
2025-10-07 12:15:33 +0200

    gpg-agent update

--- .bashrc.d/gpg-agent.sh
+++ .bashrc.d/gpg-agent.sh
@@ -7,10 +7,8 @@ alias gpg-agent-kill='echo killagent | gpg-connect-agent'
 alias gpg-agent-reload='echo killagent | gpg-connect-agent; sleep 1; echo reloadagent | gpg-connect-agent'
 alias gpg-agent-clear='gpg-agent-ls | head -n-1 | cut -d" " -f2-3 | sed "s/KEYINFO/DELETE_KEY/" | gpg-connect-agent'
 
-# export GPG_TTY="$(tty)"
+export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 unset SSH_AGENT_PID
 
-# if ! pgrep -x -u "$USER" gpg-agent &>/dev/null; then
-#   gpg-connect-agent updatestartuptty /bye &>/dev/null
-# fi
+gpg-connect-agent updatestartuptty /bye &>/dev/null
